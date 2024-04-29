d4de7be524404b0d151c3a267ab1684a687e7e62
JaviZum <134225767+JaviZum@users.noreply.github.com>
2024-04-29 10:26:32 +0200

    Update ejercicio checkpoint7.js

--- ejercicio checkpoint7.js
+++ ejercicio checkpoint7.js
@@ -1,3 +1,9 @@
+//Cree una función JS que acepte 4 argumentos. 
+//Suma los dos primeros argumentos, luego los dos segundos y multiplícalos. 
+//Si el número creado es mayor que 50, la consola registra "¡El número es mayor que 50!". 
+//Si es más pequeño, la consola registra "¡El número es menor que 50!"
+
+
 function sumaParesMultiplicaResultados(a, b, c, d) { //Generamos la función con los 4 argumentos solicitados
    
     const suma1 = a + b;                             //Asignamos la suma de los 2 primeros argumentos a suma1. 
